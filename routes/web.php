abe513036c574199933b6c1cf475205a33ade8bf
Danilo Santos <danilondosantos@gmail.com>
2024-05-02 21:35:21 -0300

    feat: :sparkles: ajustar rota e seeder para usuário

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('admin.template.app');
+    return view('site.welcome');
 });
 
 Route::get('/dashboard', function () {
