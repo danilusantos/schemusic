7451000091d0fc2b7c89814d302f041ae063d3d5
Guilherme Correia <gtavares739@gmail.com>
2024-05-02 21:38:03 -0300

    refactor: :recycle: refatorar footer

--- resources/views/admin/template/app.blade.php
+++ resources/views/admin/template/app.blade.php
@@ -25,7 +25,6 @@
         <!-- Main Sidebar Container -->
         @include('admin.template.menu')
 
-
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
             <!-- Content Header (Page header) -->
@@ -128,16 +127,7 @@
         </aside> --}}
         <!-- /.control-sidebar -->
 
-        <!-- Main Footer -->
-        <footer class="main-footer">
-            <!-- To the right -->
-            <div class="float-right d-none d-sm-inline">
-                Anything you want
-            </div>
-            <!-- Default to the left -->
-            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights
-            reserved.
-        </footer>
+        @include('admin.template.footer')
     </div>
     <!-- ./wrapper -->
 
