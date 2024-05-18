ecde0b3e38aa60a76f0a2dd19746951ae8ce8b6c
Danilo Santos <danilondosantos@gmail.com>
2024-05-18 19:52:44 -0300

    fix: :bug: remover dependência não instalada por npm

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 import "./bootstrap";
 import "moment";
 import "laravel-datatables-vite";
-import "jquery-validation";
-import "jquery-validation/dist/localization/messages_pt_BR";
+// import "jquery-validation";
+// import "jquery-validation/dist/localization/messages_pt_BR";
