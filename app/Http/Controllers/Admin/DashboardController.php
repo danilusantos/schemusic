2b9d4a31d983a5d7092afe1db4ba6a23c228afe2
Danilo Santos <danilondosantos@gmail.com>
2024-06-26 12:43:47 -0300

    feat: :sparkles: adicionar contadores ao dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,14 +3,34 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Group;
+use App\Models\Plan;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
     public $viewPath = 'admin.dashboard.';
 
+    public function __construct(
+        protected User $userModel,
+        protected Group $groupModel,
+        protected Plan $planModel
+    ) {
+        //
+    }
+
     public function index()
     {
-        return view($this->viewPath . 'index');
+        $groups = $this->groupModel->select('id')->count();
+        $plans  = $this->planModel->select('id')->count();
+        $users  = $this->userModel->select('id')->count();
+
+        $data = [
+            'groups' => $groups,
+            'plans' => $plans,
+            'users' => $users
+        ];
+
+        return view($this->viewPath . 'index', compact('data'));
     }
 }
---
+++ resources/views/components/card-count-dashboard.blade.php
@@ -0,0 +1,12 @@
+<div class="card bg-{{ $color }}">
+    <div class="card-header border-0">
+        <h5 class="card-title">
+            {{ $title }}
+        </h5>
+    </div>
+    <div class="card-body text-end">
+        <h1 class="fw-bolder">
+            {{ $value }}
+        </h1>
+    </div>
+</div>
